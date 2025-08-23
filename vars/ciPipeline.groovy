3c161d232bc532357a71f4f96d01cfd9550d03fe
Neelima137 <devadineelima137@gmail.com>
2025-08-24 04:44:33 +0530

    Update ciPipeline.groovy

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -46,30 +46,32 @@ def call(Map config = [:]) {
     //     }
     // }
 
+if (!fileExists('Dockerfile')) {
+    echo "Dockerfile not found. Skipping Docker build, push, and scan stages."
+} else {
     stage('Docker Build') {
         sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
     }
 
-  
-
-stage('Docker Push - DockerHub') {
-    withDockerRegistry([credentialsId: 'docker-cred', url: 'https://index.docker.io/v1/']) {
-        sh """
-            docker tag ${IMAGE_NAME}:${IMAGE_TAG} devadineelima137/microservice:${IMAGE_TAG}
-            docker push devadineelima137/microservice:${IMAGE_TAG}
-        """
+    stage('Docker Push - DockerHub') {
+        withDockerRegistry([credentialsId: 'docker-cred', url: 'https://index.docker.io/v1/']) {
+            sh """
+                docker tag ${IMAGE_NAME}:${IMAGE_TAG} devadineelima137/microservice:${IMAGE_TAG}
+                docker push devadineelima137/microservice:${IMAGE_TAG}
+            """
+        }
     }
-}
 
     stage('Docker Push - AWS ECR') {
-    withAWS(credentials: 'aws-creds', region: "${AWS_REGION}") {
-        sh """
-          aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
-          docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
-          docker push ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
-        """
+        withAWS(credentials: 'aws-creds', region: "${AWS_REGION}") {
+            sh """
+                aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
+                docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
+                docker push ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
+            """
+        }
     }
-}
+
     stage('Trivy Scan') {
         sh "trivy image ${IMAGE_NAME}:${IMAGE_TAG} > trivy-report.txt || true"
     }
